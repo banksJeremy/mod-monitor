f34fdec9deac8efdd8944f7c90406832ca099bb0
Jeremy Banks <j@jeremybanks.ca>
2015-04-26 18:20:33 -0700

    Initial commit - misguided tangential

---
+++ src/main.js
@@ -0,0 +1,5 @@
+setTimeout(function main() {
+  const deletionMonitor = new DeletionMonitor;
+  deletionMonitor.update();
+
+});
---
+++ src/minor-polyfills.js
@@ -0,0 +1,1 @@
+Array.from = Array.from || (x => Array.prototype.slice.apply(x));
